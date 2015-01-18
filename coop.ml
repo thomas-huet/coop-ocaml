b10889ef543829ccbd5d397da1aecc448fadbc1f
Thomas HUET <git@thomash.fr>
2015-01-18 20:10:10 +0100

    Add IO

--- coop.ml
+++ coop.ml
@@ -56,3 +56,47 @@ let wait s =
   coop
 
 let cancel coop = coop := Cancelled
+
+type key = int
+
+let watchers = Hashtbl.create 42
+
+let rec main coop = match !coop with
+| Cancelled -> None
+| Done x -> Some x
+| Waiting _ -> Hashtbl.iter (fun _ f -> f ()) watchers; main coop
+
+let new_key =
+  let n = ref 0 in
+  fun () -> (incr n; !n)
+
+module Io = struct
+  type in_channel = Pervasives.in_channel
+
+  let open_in = open_in_gen [Open_nonblock] 0
+
+  let stdin = open_in "/dev/stdin"
+
+  let read ic len =
+    let buf = Bytes.create len in
+    try
+      let real = input ic buf 0 len in
+      ref (Done (Bytes.sub_string buf 0 real))
+    with Sys_blocked_io ->
+      let coop = ref (Waiting (Queue.create ())) in
+      let key = new_key () in
+      let watcher () =
+	match !coop with
+	| Cancelled -> Hashtbl.remove watchers key
+        | Done _ -> assert false
+	| Waiting q -> try
+          let real = input ic buf 0 len in
+          let s = Bytes.sub_string buf 0 real in
+          Hashtbl.remove watchers key;
+          coop := Done s;
+          broadcast q s
+        with Sys_blocked_io -> ()
+    in
+    Hashtbl.add watchers key watcher;
+    coop
+end
--- coop.mli
+++ coop.mli
@@ -2,6 +2,8 @@ type 'a t
 
 type 'a signal
 
+type key
+
 val return : 'a -> 'a t
 
 val bind : 'a t -> ('a -> 'b t) -> 'b t
@@ -17,3 +19,19 @@ val signal : 'a signal -> 'a -> unit
 val broadcast : 'a signal -> 'a -> unit
 
 val cancel : 'a t -> unit
+
+val main : 'a t -> 'a option
+
+val new_key : unit -> key
+
+val watchers : (key, unit -> unit) Hashtbl.t
+
+module Io : sig
+  type in_channel
+
+  val stdin : in_channel
+
+  val open_in : string -> in_channel
+
+  val read : in_channel -> int -> string t
+end
